d9bcfe1067ea630079b8444d98ec9d0ef447c387
niklasschmitz <niklas.f.schmitz@gmail.com>
2019-01-28 22:18:44 +0100

    add dims in DQN

--- model.py
+++ model.py
@@ -1,15 +1,15 @@
 import jax.numpy as np
 from jax.experimental import stax
-from jax.experimental.stax import Dense, GeneralConv, Relu
+from jax.experimental.stax import Dense, Conv, Relu, Flatten
 
 
 def DeepQNetwork():
     weights_init, predict = stax.serial(
-        GeneralConv(), Relu,
-        GeneralConv(), Relu,
-        GeneralConv(), Relu,
+        Conv(32, (8,8)), Relu,
+        Conv(64, (4,4)), Relu,
+        Conv(128, (3,3)), Relu,
         Dense(512), Relu,
-        stax.Flatten(),
+        Flatten,
         Dense(6)
     )
     return weights_init, predict
