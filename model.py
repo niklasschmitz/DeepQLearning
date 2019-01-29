09970130c69eacb5b4ae3e0935441b75613db257
niklasschmitz <niklas.f.schmitz@gmail.com>
2019-01-29 16:38:16 +0100

    remove main from model.py

--- model.py
+++ model.py
@@ -5,15 +5,11 @@ from jax.experimental.stax import Dense, Conv, Relu, Flatten
 
 def DeepQNetwork():
     weights_init, predict = stax.serial(
-        Conv(32, (8,8)), Relu,
-        Conv(64, (4,4)), Relu,
-        Conv(128, (3,3)), Relu,
-        Dense(512), Relu,
+        Conv(32, (8, 8)), Relu,
+        Conv(64, (4, 4)), Relu,
+        Conv(128, (3, 3)), Relu,
         Flatten,
+        Dense(512), Relu,
         Dense(6)
     )
     return weights_init, predict
-
-
-def main():
-    pass
