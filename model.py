0daae9648ab8cf836e589dd5a88e26d44b079930
niklasschmitz <niklas.f.schmitz@gmail.com>
2019-02-09 13:44:44 +0100

    shrink DQN size

--- model.py
+++ model.py
@@ -4,11 +4,11 @@ from jax.experimental.stax import Dense, Conv, Relu, Flatten
 
 def DeepQNetwork():
     init_fun, predict_fun = stax.serial(
-        Conv(32, (8, 8)), Relu,
-        Conv(64, (4, 4)), Relu,
-        Conv(128, (3, 3)), Relu,
+        Conv(16, (8, 8), strides=(4, 4)), Relu,
+        Conv(32, (4, 4), strides=(2, 2)), Relu,
+        Conv(64, (3, 3)), Relu,
         Flatten,
-        Dense(512), Relu,
+        Dense(256), Relu,
         Dense(6)
     )
     return init_fun, predict_fun
